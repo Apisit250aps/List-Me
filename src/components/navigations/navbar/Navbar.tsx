52fa78b6dfdc7117aa7ac98108e1ba2317afe303
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-09-15 10:16:59 +0700

    width

--- src/components/navigations/navbar/Navbar.tsx
+++ src/components/navigations/navbar/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar(prop: NavbarProp) {
           </div>
           <ul
             tabIndex={0}
-            className="menu w-auto menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu w-auto menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 p-2 shadow"
           >
             <li>
               <a className="justify-between">
